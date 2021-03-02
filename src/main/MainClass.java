2863715fc49b587398c1fc1cb5ad5b2d655fea67
Your Name <you@example.com>
2021-03-02 17:55:08 +0900

    기본 프로젝트 생성

---
+++ src/main/MainClass.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("main인 시작");
+		// kim - 로그인관련 기능 만드세요
+		// cho - 회원 관리 기능을 만드세요
+		Scanner input = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.print("1.로그인 2.회원관리");
+			num = input.nextInt();
+			switch(num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			}
+		}
+	}
+
+}
